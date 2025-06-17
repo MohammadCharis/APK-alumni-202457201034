34ff67d491f18f1a2751b79cbdb1083e05c41273
charis <charis@gmail.com>
2025-06-17 08:00:23 +0700

    commit

---
+++ src/alumni202457201034/Int.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alumni202457201034;
+
+/**
+ *
+ * @author LENOVO
+ */
+class Int {
+    
+}
---
+++ src/alumni202457201034/alumni202457201034.java
@@ -0,0 +1,13 @@
+package alumni202457201034;
+
+
+public class alumni202457201034 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
